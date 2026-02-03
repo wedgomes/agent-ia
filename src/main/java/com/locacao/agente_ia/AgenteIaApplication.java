c39a3e4f02d8c0259e0d5aaf66a63922254b22bc
Wédson Gomes <wedson_mxt@hotmail.com>
2026-02-02 21:14:51 -0300

    first commit

---
+++ src/main/java/com/locacao/agente_ia/AgenteIaApplication.java
@@ -0,0 +1,13 @@
+package com.locacao.agente_ia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenteIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenteIaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/locacao/agente_ia/AgenteIaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.locacao.agente_ia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenteIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
